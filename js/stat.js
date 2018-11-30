aaa1017680d13497cbcd83a0c45d01d527026398
Radja7 <Radja7@users.noreply.github.com>
2018-11-30 12:00:59 +0300

    Оптимизировал код

--- js/stat.js
+++ js/stat.js
@@ -3,109 +3,71 @@ var CLOUD_WIDTH = 520;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 50;
-var FONT_GAP_X = 50;
-var FONT_GAP_Y = 10;
-var MAX_HEIGHT_HISTOGRAM = 150;
-var WIDTH_HISTOGRAM = 40;
-var DISTANCE_BETWEEN_HISTOGRAM = 50;
-var PLAYER_COLOR_HISTOGRAM = 'rgba(255, 0, 0, 1)';
+var GAP = 10;
+var CLOUD_BACKGROUND_COLOR = '#fff';
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var FONT = '16px PT Mono';
+var BASELINE = 'hanging';
+var FIRST_LINE_TEXT = 'Ура вы победили!';
+var SECOND_LINE_TEXT = 'Список результатов:';
+var TEXT_LINE_X = 140;
+var TEXT_LINE_Y = 40;
+var TEXT_LINE2_Y = TEXT_LINE_Y + GAP * 2;
+var WIDTH_GRAPH = 40;
+var MAX_GRAPH_HEIGHT = 150;
+var TEXT_SPACE = 100;
+var GRAPH_GAP = 50;
+var PLAYER_COLOR_GRAPH = 'rgba(255, 0, 0, 1)';
+
 
 window.prevSteps = function(){}
 window.renderStatistics = function(ctx, names, times){
 
-  //var  times = times;
+  // Функция которая возращает лучший результат игры
+  var getBestResult = function () {
+    return Math.max.apply(null, times);
+  };
+  var maxResult = getBestResult(times);
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
-  ctx.fillRect(CLOUD_X + 10, CLOUD_Y + 10, CLOUD_WIDTH, CLOUD_HEIGHT);
+  // Поучаем цвет гистограмы других игроков или своей.
+  var getPlayerGraphColor = function (name) {
+    return (name === 'Вы') ? PLAYER_COLOR_GRAPH : 'hsla(235, 100%, ' + Math.random() * 100 + '% , 1)';
+  };
 
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  // Функция рисует гистограмы игроков их имена и результаты
+  var drawPlayerBars = function () {
+    var nextCoordinateX = 155;
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 150, 40);
+    for (var i = 0; i < times.length; i++) {
+      var playerHeightBar = Math.round((times[i] / maxResult) * MAX_GRAPH_HEIGHT);
+      var nextCoordinateY = TEXT_SPACE + MAX_GRAPH_HEIGHT - playerHeightBar;
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Список результатов:', 150, 70);
+      drawBlock(nextCoordinateX, nextCoordinateY, WIDTH_GRAPH, playerHeightBar, getPlayerGraphColor(names[i])); // нарисовал гистограммы
+      drawText(FONT, BASELINE, Math.round(times[i]), nextCoordinateX, (nextCoordinateY - GAP * 2)); // написал время
+      drawText(FONT, BASELINE, names[i], nextCoordinateX, (CLOUD_HEIGHT - GAP)); // написал имена
 
-  drawHistogram(names);
-
-  function getHistogram(times){
-    var heightColumns = [];
-    var proportion;
-    var maxTime = +times[0];
-    if(isNaN(+times[0]) === false){
-      for(var i = 1; i < times.length; i++) {
-        if(maxTime < +times[i]) {
-          maxTime = +times[i];
-        }
-      }
-      maxTime = Math.floor(maxTime);
-    } else {
-      maxTime = 0;
+      nextCoordinateX += GRAPH_GAP + WIDTH_GRAPH;
     }
+  };
 
-    proportion = maxTime / MAX_HEIGHT_HISTOGRAM;
+  // Функция рисует облако
+  var drawBlock = function (x, y, width, height, fillStyle) {
+    ctx.fillStyle = fillStyle;
+    ctx.fillRect(x, y, width, height);
+  };
 
-    for(var i = 0; i < times.length; i++) {
-      heightColumns.push(Math.floor(+times[i] / proportion));
-    }
+  // Функция рисует текст
+  var drawText = function (font, baseline, text, x, y) {
+    ctx.font = font;
+    ctx.textBaseline = baseline;
+    ctx.strokeText(text, x, y);
+  };
 
-    return heightColumns;
-
-  }
-
-  function drawHistogram(names){
-    var heightColumns = getHistogram(times);
-    var cloudContentBottom = CLOUD_HEIGHT + CLOUD_Y - FONT_GAP_Y;
-    var flag = true;
-
-    for(var i = 0, k = 1; i < names.length; i++, k++){
-
-      if(names[i] === 'Вы'){
-
-        // histograms
-        ctx.fillStyle = PLAYER_COLOR_HISTOGRAM;
-        ctx.fillRect((CLOUD_X + FONT_GAP_X), cloudContentBottom - heightColumns[i] - 30, WIDTH_HISTOGRAM, heightColumns[i]);
-
-        // name players
-        ctx.font = '16px PT Mono';
-        ctx.fillStyle = '#000';
-        ctx.fillText(names[i], (CLOUD_X + FONT_GAP_X), cloudContentBottom - 10);
-
-        // time players
-        ctx.font = '16px PT Mono';
-        ctx.fillStyle = '#000';
-        ctx.fillText(Math.floor(times[i]), (CLOUD_X + FONT_GAP_X), cloudContentBottom - heightColumns[i] - 35);
-
-        k--;
-      } else {
-        // histograms
-        var randomCount = Math.random().toFixed(2);
-        ctx.fillStyle = 'rgba(0, 0, 255,' + randomCount + ')';
-
-        ctx.fillRect((CLOUD_X + FONT_GAP_X) + (WIDTH_HISTOGRAM + DISTANCE_BETWEEN_HISTOGRAM) * k, cloudContentBottom - heightColumns[i] - 30, WIDTH_HISTOGRAM, heightColumns[i]);
-
-        // name players
-        ctx.font = '16px PT Mono';
-        ctx.fillStyle = '#000';
-        ctx.fillText(names[i], (CLOUD_X + FONT_GAP_X) + (WIDTH_HISTOGRAM + DISTANCE_BETWEEN_HISTOGRAM) * k, cloudContentBottom - 10);
-
-        // time players
-        ctx.font = '16px PT Mono';
-        ctx.fillStyle = '#000';
-        ctx.fillText(Math.floor(times[i]), (CLOUD_X + FONT_GAP_X) + (WIDTH_HISTOGRAM + DISTANCE_BETWEEN_HISTOGRAM) * k, cloudContentBottom - heightColumns[i] - 35);
-      }
-
-    }
-
-  }
-
-  function randomCount(){
-
-  }
+  drawBlock(CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR); // нарисовали тень в облаке
+  drawBlock(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_BACKGROUND_COLOR); // нарисовали облако
+  drawText(FONT, BASELINE, FIRST_LINE_TEXT, TEXT_LINE_X, TEXT_LINE_Y); // написали 1 строку текста
+  drawText(FONT, BASELINE, SECOND_LINE_TEXT, TEXT_LINE_X, TEXT_LINE2_Y); // написали 2 строку текста
+  drawPlayerBars(); // нарисовали гистограммы с результатами
 
 }
 
